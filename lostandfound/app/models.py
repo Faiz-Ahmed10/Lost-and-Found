8edc8fd117c1848fce9fdccc96b418c8879f833e
ahmedrafiuddin360 <ahmedrafiuddin520@gmail.com>
2023-11-27 19:52:53 +0530

    register connected with postgre database user insertion on cloud databse on new model users successfully

---
+++ lostandfound/app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+# from django.contrib.auth.models import User
+# Create your models here.
+class Users(models.Model):
+      user = models.CharField(max_length=128,blank=True)
+
+      email = models.EmailField(blank=True, unique=True)
+
+      password = models.CharField(max_length=128, blank=True)
+
+        # Use a PositiveIntegerField for a unique ID with a maximum of 10 digits
+    #   id = models.PositiveIntegerField(primary_key=True, unique=True, editable=False)
+      id = models.CharField(primary_key=True, max_length=200)
+      def __str__(self):
+        return f'{self.user} ({self.id})'
--- lostandfound/lostandfound/settings.py
+++ lostandfound/lostandfound/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app",
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,15 @@ WSGI_APPLICATION = "lostandfound.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'railway',
+    'USER': 'postgres',
+    'PASSWORD': 'g2bEFA116a1bf4CF61eEaea6g-b*16G2',
+    'HOST': 'monorail.proxy.rlwy.net',
+    'PORT': '43480',
+    'OPTIONS': {'sslmode': 'require'},
+  }
 }
 
 
